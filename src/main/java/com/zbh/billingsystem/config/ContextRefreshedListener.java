64edc0cd5c1534789cd6bb5105b89ee902e20060
SIPDOMAIN\ZinBhoneHtut <zinbhonehtut.sip@gmail.com>
2023-08-23 10:07:44 +0630

    refactor (import) : remove unused import

--- src/main/java/com/zbh/billingsystem/config/ContextRefreshedListener.java
+++ src/main/java/com/zbh/billingsystem/config/ContextRefreshedListener.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
